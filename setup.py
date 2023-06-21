7e56e4ec85947ddc608758e82128b47df8f2d0b0
Arthur Crawford <arthur.crawford@gmail.com>
2023-06-21 10:42:02 +0100

    adding missing module to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     name='zephr',
-    version='0.1.1',
-    py_modules=['zephr'],
+    version='0.1.2',
+    py_modules=['zephr', 'api_auth.py'],
     install_requires=[
         'click',
+        'requests',
+        'keyring',
+        'pwinput'
     ],
     entry_points={
         'console_scripts': [
