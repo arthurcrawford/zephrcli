2c71d2a543760a74a5c45daea89f4700afa58cda
Arthur Crawford <arthur.crawford@gmail.com>
2023-06-21 10:08:24 +0100

    added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='zephr',
+    version='0.1',
+    py_modules=['zephr'],
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'zephr = zephr:cli',
+        ],
+    },
+)
