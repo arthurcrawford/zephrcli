0d27188b6e329b00cae60cac4bd01f7b4973d9e0
Arthur Crawford <arthur.crawford@gmail.com>
2023-06-22 16:21:24 +0100

    added VERSION file to distribution

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ import os
 
 # Load version from VERSION file
 # version = importlib.resources.read_text('zephrcli', "VERSION")
-version = '0.1.24'
-print(f'Setup.py - Version: {version}')
+# version = '0.1.24'
 print(f'Setup.py - CWD: {os.getcwd()}')
 print(f'Setup.py - __file__: {__file__}')
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 print(f'Setup.py - ROOT_DIR: {ROOT_DIR}')
 VERSION_PATH = os.path.join(ROOT_DIR, 'src/zephrcli')
 print(f'Setup.py - ROOT_DIR: {VERSION_PATH}')
+version = 'Unknown'
 try:
     with open(os.path.join(VERSION_PATH, 'VERSION')) as version_file:
-        version_from_file = version_file.read().strip()
-    print(f'Setup.py - Version from file: {version_from_file}')
+        version = version_file.read().strip()
     # No packages yet so can't do this
     test = importlib.resources.read_text('zephrcli', "VERSION")
 except Exception as e:
-    raise 'An error occurred loading file'
+    raise 'An error occurred reading VERSION file'
+print(f'Setup.py - Version: {version}')
 
 setup(
     name='zephrcli',
