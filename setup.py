b79e39c4d43d548df63c9553e48c6241a1319eab
Arthur Crawford <arthur.crawford@gmail.com>
2023-06-22 16:04:33 +0100

    added VERSION file to distribution

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ import os
 
 # Load version from VERSION file
 # version = importlib.resources.read_text('zephrcli', "VERSION")
-version = '0.1.22'
+version = '0.1.23'
 print(f'Setup.py - Version: {version}')
 print(f'Setup.py - CWD: {os.getcwd()}')
 print(f'Setup.py - __file__: {__file__}')
-
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+print(f'Setup.py - ROOT_DIR: {ROOT_DIR}')
+VERSION_PATH = os.path.join(ROOT_DIR, 'src/zephrcli')
+print(f'Setup.py - ROOT_DIR: {VERSION_PATH}')
 try:
     test = importlib.resources.read_text('zephrcli', "VERSION")
 except Exception as e:
