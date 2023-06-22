95e00a3fc07e000031328ba833bb2c61fa1d501d
Arthur Crawford <arthur.crawford@gmail.com>
2023-06-22 15:44:38 +0100

    added VERSION file to distribution

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import os
 
 # Load version from VERSION file
 # version = importlib.resources.read_text('zephrcli', "VERSION")
-version = '0.1.20'
+version = '0.1.21'
 print(f'Setup.py - Version: {version}')
+print(f'Setup.py - CWD: {os.getcwd()}')
 
 try:
     test = importlib.resources.read_text('zephrcli', "VERSION")
 except Exception as e:
-    cwd = f'OS Current WD: {os.getcwd()}'
-    raise f'An error occurred loading from dir.  CWD: {cwd}'
+    raise 'An error occurred loading file'
 
 setup(
     name='zephrcli',
